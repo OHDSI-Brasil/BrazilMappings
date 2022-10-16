7f52e2b5dd392ac348123fba56445ca5629312a5
fabkury <fab@kury.dev>
2022-10-16 14:42:06 -0400

    Zelador 340ae5205f4158d3e3f348159f026dda

--- baixa/zelador.R
+++ baixa/zelador.R
@@ -6,27 +6,36 @@
 #' Início: 2022-08-11 20:47-4.
 #' 
 
-# Controle --------------------------------------------------------------------------------------------------------
-faz_executa <- TRUE
-
+# Variáveis (espinha) ---------------------------------------------------------------------------------------------
+usuário_admin <- 'sigtap_omop_admin'
 bibliotecas_global <- c('tidyverse', 'dbplyr', 'processx', 'ps', 'DBI', 'RMariaDB')
 dolt_dir <- suppressWarnings(normalizePath('./dolt'))
 dolt_exe <- paste0(dolt_dir, '/dolt.exe')
 arq_config <- './config.txt'
-dir_linhas <- './linhas'
-dir_linhas_sobe <- paste0(dir_linhas, '/upload')
-quórum_mínimo <- 3
-limite_distribuição <- 5
-usuário_admin <- 'sigtap_omop_admin'
-pegar_linhas_min <- 2
-pegar_linhas_max <- 50
-
-# Colunas a atualizar quando o participante sobe linhas.
-colunas_a_subir <- c("mappingStatus", "equivalence", "statusSetOn", "conceptId", "conceptName", "domainId",
-  "mappingType", "comment", "createdBy", "createdOn", "assignedReviewer")
+no_versão_zelador <- '10-16'
 
 
-# Funções ---------------------------------------------------------------------------------------------------------
+# Funções (espinha) -----------------------------------------------------------------------------------------------
+lê_config <- function(endr) {
+# Lê config.txt ---------------------------------------------------------------------------------------------------
+  if(!file.exists(endr))
+    stop(paste0('Não foi possível encontrar o arquivo config.txt. Favor verificar a instalação.\n',
+      'Arquivo não encontrado:\n', endr))
+  
+  config_txt <- readLines(endr, warn = FALSE)
+  
+  config <- str_match(config_txt, '(\\S+) = (\\S+)') |>
+    `colnames<-`(c('string', 'variável', 'valor')) |>
+    as_tibble()
+  
+  assign('config', config, envir = .GlobalEnv)
+  pwalk(config, ~assign(..2, ..3, envir = .GlobalEnv))
+  
+# Pós-processamento -----------------------------------------------------------------------------------------------
+  # Cria repo_dir
+  assign('repo_dir', paste0(dolt_dir, '\\', proj_repo), envir = .GlobalEnv)
+}
+
 ensureDir <- function(...) {
   d <- paste0(...)
   if(!dir.exists(d))
@@ -51,17 +60,6 @@ fecha_stdin <- function() {
       remove('entrada', envir = .GlobalEnv)
     }
   }
-}
-
-dolt <- function(..., script = dolt_exe, verbose = FALSE) {
-  dolt_proc <- process$new(script, args = c(...), stdout = "|", stderr = '|', wd = dolt_dir)
-  res <- dolt_proc$read_all_output()
-  if(verbose) {
-    err <- dolt_proc$read_all_error()
-    if(nchar(err[1]) > 0)
-      console('Erro: ', err)
-  }
-  res
 }
 
 consolex <- function(...) {
@@ -102,96 +100,6 @@ carrega_bibliotecas <- function(bibliotecas = bibliotecas_global) {
   console('Carregando bibliotecas.')
   for(biblioteca in bibliotecas)
     suppressWarnings(suppressPackageStartupMessages(library(biblioteca, character.only = TRUE)))
-}
-
-fecha_dolt_sql <- function() {
-  if(exists('proc_sql', envir = .GlobalEnv)) {
-    if(exists('dbd', envir = .GlobalEnv)) {
-      # Desconectar antes de fechar o Dolt.
-      dbDisconnect(dbd)
-      remove('dbd', envir = .GlobalEnv)
-    }
-    
-    prsql <- get('proc_sql', envir = .GlobalEnv)
-    prsql$kill()
-    remove('proc_sql', envir = .GlobalEnv)
-  }
-}
-
-abre_dolt_sql <- function(verbose = FALSE) {
-  processos <- ps()
-  máscara <- processos$name == 'dolt.exe'
-  if(any(máscara)) {
-    if(verbose) {
-      message('Dolt.exe já aberto em:')
-      print(processos[máscara,])
-    }
-  } else {
-    # if(verbose)
-      console('Executando o Dolt.')
-    assign('proc_sql', envir = .GlobalEnv,
-      process$new(dolt_exe, c('sql-server'), wd = dolt_dir, supervise = TRUE))
-    
-    # Esperar para conectar via SQL. Não sei se isto é necessário.
-    Sys.sleep(1)
-  }
-}
-
-lê_config <- function(endr) {
-  config_txt <- readLines(endr, warn = FALSE)
-  
-  config <- str_match(config_txt, '(\\S+) = (\\S+)') |>
-    `colnames<-`(c('string', 'variável', 'valor')) |>
-    as_tibble()
-  
-  assign('config', config, envir = .GlobalEnv)
-  pwalk(config, ~assign(..2, ..3, envir = .GlobalEnv))
-}
-
-obtém_creds <- function(sobrsc = FALSE) {
-  if(sobrsc || is.null(.GlobalEnv$creds)) {
-    .GlobalEnv$creds <- suppressWarnings(dolt('creds', 'check') |>
-      strsplit('\n') |>
-      unlist())
-    if(verifica_creds(creds)) {
-      # Obtém o nome de usuário.
-      .GlobalEnv$username <- str_match(creds[6], ' User: (.+)')[,2]
-    }
-  }
-  .GlobalEnv$creds
-}
-
-verifica_creds <- function(increds = character(0)) {
-  if(length(increds) == 0)
-    increds <- obtém_creds()
-  return('Success.' %in% increds)
-}
-
-username_do_creds <- function() {
-  if(is.null(.GlobalEnv$username)) {
-    console('Obtendo nome de usuário.')
-    creds <- obtém_creds()
-    .GlobalEnv$username <- str_match(creds[6], ' User: (.+)')[,2]
-  }
-  .GlobalEnv$username
-}
-
-conecta_mariadb <- function() {
-  if(!exists('dbd', envir = .GlobalEnv)) {
-    console('Conectando via SQL.')
-    library(RMariaDB)
-    library(DBI)
-    .GlobalEnv$dbd <- dbConnect(RMariaDB::MariaDB(), host = "localhost", user = "root")
-  }
-  dbd
-}
-
-gera_id_reserva <- function() {
-  # idr <- as.character(round(runif(1, 1, 10000000)))
-  # if(nchar(idr) > 5)
-  #   idr <- substr(idr, 1, 5)
-  # idr
-  format(Sys.time(), '%m-%d-%H-%M')
 }
 
 pede_comando <- function(comandos, mensagem, permite_zero = TRUE) {
@@ -241,46 +149,153 @@ pede_enter <- function() {
   fecha_stdin()
 }
 
-add_commit_push <- function(commit_message, pede_pull_request = FALSE) {
-  continuar <- TRUE
+pede_pull_request <- function() {
+  # Abre o navegador na página de criar pull request.
+  url_pull_request <- 'https://www.dolthub.com/repositories/ohdsi-brasil/sigtap_omop/pulls/new?refName=main'
+  console('Aperte Enter para abrir o navegador na página para fazer o pull request:\n', url_pull_request, '.')
+  pede_enter()
+  console('Abrindo o navegador. Por favor preencha os campos e confirme o pull request.')
+  browseURL(url_pull_request)
+  Sys.sleep(4) # Apenas para minimizar a chance de que novo texto aparecerá na tela antes que o navegador abra.
+}
+
+add_commit_push <- function(commit_message, pedir_pull_request = FALSE) {
   tryCatch({
-    console('Executando dolt add.')
+    console('Produzindo dolt push.')
     dbExecute(dbd, "call dolt_add('.');")
-    
-    console('Executando dolt commit.')
     dbExecute(dbd, paste0("call dolt_commit('-m', '", commit_message, "');"))
+    dbExecute(dbd, "call dolt_push();")
     
-    console('Executando dolt push.')
-    dbExecute(dbd, "call dolt_push('--set-upstream', 'origin', 'main');")
+    console('Dolt push completo.')
+    ## Gerar o pull request.
+    if(pedir_pull_request)
+      pede_pull_request()
   }, error = \(e) {
-    console('Erro ao executar dolt push.')
-    continuar <<- FALSE
+    console('Erro ao executar dolt push:')
+    print(e)
   })
-  
-  if(!continuar)
-    return()
-  
-  ## Gerar o pull request.
-  if(pede_pull_request) {
-    # Abre o navegador na página de criar pull request.
-    url_pull_request <- 'https://www.dolthub.com/repositories/ohdsi-brasil/sigtap_omop/pulls/new?refName=main'
+}
+
+conecta_mariadb <- function() {
+  if(!exists('dbd', envir = .GlobalEnv)) {
+    console('Conectando via SQL.')
+    library(RMariaDB)
+    library(DBI)
+    .GlobalEnv$dbd <- dbConnect(RMariaDB::MariaDB(), host = "localhost", user = "root")
+  }
+  .GlobalEnv$dbd
+}
+
+dolt <- function(..., wd = dolt_dir, verbose = TRUE) {
+  proc_res <- processx::run(command = dolt_exe, args = c(...), wd = wd, error_on_status = FALSE)
+  if(proc_res$status != 0) {
+    if(nchar(proc_res$stderr[1]) > 0)
+      console('Erro:\n', proc_res$stderr)
+  }
+  proc_res
+}
+
+fecha_dolt_sql <- function() {
+  if(exists('proc_sql', envir = .GlobalEnv)) {
+    if(exists('dbd', envir = .GlobalEnv)) {
+      # Desconectar antes de fechar o Dolt.
+      dbDisconnect(dbd)
+      remove('dbd', envir = .GlobalEnv)
+    }
     
-    console('Aperte Enter para abrir o navegador na página para fazer o pull request:\n', url_pull_request, '\n\n',
-      'Por favor lembre-se que este pull request é obrigatório.')
-    
-    pede_enter()
-    
-    console('Abrindo o navegador. Por favor preencha os campos e confirme o pull request.')
-    
-    browseURL(url_pull_request)
-    
-    Sys.sleep(4) # Apenas para minimizar a chance de que novo texto aparecerá na tela antes que o navegador abra.
+    prsql <- get('proc_sql', envir = .GlobalEnv)
+    prsql$kill()
+    remove('proc_sql', envir = .GlobalEnv)
   }
 }
 
-apaga_vista <- function(nome_vista) {
-  dbExecute(dbd, paste0('drop view ', nome_vista, ';'))
-  add_commit_push(paste0('Apaga vista ', nome_vista, '.'))
+abre_dolt_sql <- function(verbose = FALSE) {
+  processos <- ps()
+  máscara <- processos$name == 'dolt.exe'
+  if(any(máscara)) {
+    if(verbose) {
+      message('Dolt.exe já aberto em:')
+      print(processos[máscara,])
+    }
+  } else {
+    # if(verbose)
+    console('Executando o Dolt.')
+    dolt_process <- process$new(dolt_exe, c('sql-server'), wd = dolt_dir, supervise = TRUE)
+    assign('proc_sql', dolt_process, envir = .GlobalEnv)
+    
+    # Esperar para conectar via SQL. Não sei se isto é necessário.
+    Sys.sleep(1)
+  }
+}
+
+
+# Controle --------------------------------------------------------------------------------------------------------
+if(!exists('faz_executa_zelador'))
+  faz_executa_zelador <- TRUE
+
+dir_linhas <- './linhas'
+dir_linhas_sobe <- paste0(dir_linhas, '/upload')
+limite_distribuição <- 5
+pegar_linhas_min <- 2
+pegar_linhas_max <- 50
+
+# Colunas a atualizar quando o participante sobe linhas.
+colunas_a_subir <- c("mappingStatus", "equivalence", "statusSetOn", "conceptId", "conceptName", "domainId",
+  "mappingType", "comment", "createdBy", "createdOn", "assignedReviewer")
+
+
+# Funções ---------------------------------------------------------------------------------------------------------
+obtém_creds <- function(sobrsc = FALSE) {
+  if(sobrsc || is.null(.GlobalEnv$creds)) {
+    credscheck <- dolt('creds', 'check')$stdout
+    .GlobalEnv$creds <- suppressWarnings(credscheck |>
+      strsplit('\n') |>
+      unlist())
+    if(verifica_creds(creds)) {
+      # Obtém o nome de usuário.
+      .GlobalEnv$username <- str_match(.GlobalEnv$creds[6], ' User: (.+)')[,2]
+    }
+  }
+  .GlobalEnv$creds
+}
+
+verifica_creds <- function(increds = character(0)) {
+  if(length(increds) == 0)
+    increds <- obtém_creds()
+  return('Success.' %in% increds)
+}
+
+gera_id_reserva <- function() {
+  # idr <- as.character(round(runif(1, 1, 10000000)))
+  # if(nchar(idr) > 5)
+  #   idr <- substr(idr, 1, 5)
+  # idr
+  format(Sys.time(), '%m-%d-%H-%M')
+}
+
+usa_repositório <- function(repo) {
+  # Confere quais databases (repositórios do DoltHub) já existem.
+  bases <- dbGetQuery(dbd, 'show databases;') |> pull(Database)
+  
+  # O Sigtap Omop já existe localmente?
+  if(!repo %in% bases) {  # proj_repo vem do lê_config()
+    # Clonar repositório.
+    repo_address <- paste0(username, '/sigtap_omop')
+    console('Clonando repositório ', repo_address, '.')
+    dbGetQuery(dbd, paste0("call dolt_clone('", repo_address, "');"))
+    
+    # Tentar novamente encontrar o Sigtap Omop na lista de repositórios locais. Se falhar, abortar.
+    bases <- dbGetQuery(dbd, 'show databases;') |> pull(Database)
+    if(!repo %in% bases) {
+      console('Falha ao clonar repositório ', repo, '.')
+      return(FALSE)
+    }
+  }
+  
+  # Troca para o sigtap_omop.
+  dbExecute(dbd, paste0('use ', proj_repo, ';'))
+  
+  return(TRUE)
 }
 
 
@@ -339,30 +354,12 @@ baixa_linhas <- function() {
   conecta_mariadb()
 
 # Acessa a database sigtap_omop; clona se preciso. ----------------------------------------------------------------
-  # Confere quais databases (repositórios do DoltHub) já existem.
-  bases <- dbGetQuery(dbd, 'show databases;') |> pull(Database)
+  if(!usa_repositório(proj_repo))
+    return()
   
-  # O Sigtap Omop já existe localmente?
-  if(!proj_repo %in% bases) {  # proj_repo vem do lê_config()
-    # Clonar repositório.
-    repo_address <- paste0(username, '/sigtap_omop')
-    console('Clonando repositório ', repo_address, '.')
-    dbGetQuery(dbd, paste0("call dolt_clone('", repo_address, "');"))
-    
-    # Tentar novamente encontrar o Sigtap Omop na lista de repositórios locais. Se falhar, abortar.
-    bases <- dbGetQuery(dbd, 'show databases;') |> pull(Database)
-    if(!proj_repo %in% bases) {
-      console('Falha ao clonar repositório.')
-      return()
-    }
-  }
-  
-  # Troca para o sigtap_omop.
-  dbExecute(dbd, paste0('use ', proj_repo, ';'))
-  
-  # Executa dolt fetch.
-  console('Atualizando a tabela local (dolt fetch).')
-  dbExecute(dbd, paste0('call dolt_fetch();'))
+  # Executa dolt pull.
+  console('Atualizando a tabela local (dolt pull).')
+  dbExecute(dbd, paste0('call dolt_pull();'))
   
   tabela_dolt <- tbl(dbd, paste0(nome_tabela, '_dolt')) # tabela lazy
   
@@ -376,9 +373,8 @@ baixa_linhas <- function() {
   linhas_aguardando <- linhas_do_usuário |>
     filter(mappingStatus == 'UNCHECKED')
   
-  n_linhas_aguardando <- linhas_aguardando |>
-    tally() |> pull(n) |> as.integer()
-  
+  n_linhas_aguardando <- linhas_aguardando |> tally() |> pull(n) |> as.integer()
+3
   if(n_linhas_aguardando > 0)
     console('Encontradas ', n_linhas_aguardando, ' linhas previamente reservadas e ainda não mapeadas.')
   
@@ -487,7 +483,7 @@ baixa_linhas <- function() {
       commit_message <- paste0('Reserva de ', n_linhas_faltantes, ' linhas para ', username,
         ' (Zelador ', id_reserva, ').')
       
-      add_commit_push(commit_message, pede_pull_request = TRUE)
+      add_commit_push(commit_message, pedir_pull_request = TRUE)
     }
   }
 
@@ -513,7 +509,9 @@ baixa_linhas <- function() {
 
 
 # Refaz push ------------------------------------------------------------------------------------------------------
-refazer_push <- function() {
+if(F) {
+  # Aposentado em 2022-10-16 14:26
+  refazer_push <- function() {
 # Prepara ---------------------------------------------------------------------------------------------------------
   # Executa o Dolt em processo separado.
   abre_dolt_sql()
@@ -538,6 +536,7 @@ refazer_push <- function() {
   
   console('Abrindo o navegador. Por favor preencha os campos e confirme o pull request.')
   browseURL(url_pr)
+}
 }
 
 
@@ -595,56 +594,14 @@ sobe_linhas <- function() {
   
 
 # Atualiza o Dolt e gera o pull request ---------------------------------------------------------------------------
-  add_commit_push(paste0('Zelador sobe ', arq_selecionado, ' de ', username, '.'), pede_pull_request = TRUE)
-}
-
-
-# Consenso --------------------------------------------------------------------------------------------------------
-cria_vista_consenso <- function() {
-  votos_por_termo <- tabela_dolt |>
-    filter(statusSetBy != usuário_admin) |>
-    group_by(sourceCode) |>
-    summarise(.groups = 'keep',
-      votos_termo = n_distinct(statusSetBy))
-  
-  votos_por_termo_conceito <- tabela_dolt |>
-    filter(statusSetBy != usuário_admin) |>
-    group_by(sourceCode, conceptId) |>
-    summarise(.groups = 'keep',
-      votos_conceito = n_distinct(statusSetBy))
-  
-  votação <- inner_join(votos_por_termo, votos_por_termo_conceito, by = 'sourceCode')
-  
-  nomes_termos <- tabela_dolt |>
-    filter(statusSetBy == usuário_admin) |>
-    select(sourceCode, sourceName)
-  
-  nomes_conceitos <- tabela_dolt |>
-    select(conceptId, conceptName) |>
-    distinct()
-  
-  resultado <- votação |>
-    mutate(
-      quórum = votos_termo >= quórum_mínimo,
-      consenso = quórum & (votos_conceito > votos_termo/2)) |>
-    inner_join(nomes_termos, by = 'sourceCode') |>
-    inner_join(nomes_conceitos, by = 'conceptId') |>
-    select(sourceCode, sourceName, votos_termo, conceptId, conceptName, votos_conceito, quórum, consenso) |>
-    arrange(desc(quórum), sourceCode, conceptId)
-  
-  sql_txt <- sql_render(resultado) |> as.character()
-  nome_vista <- paste0(nome_tabela, '_consenso')
-  sql_txt <- paste0('create view ', nome_vista, ' as\n', sql_txt, ';')
-  dbExecute(dbd, sql_txt)
-  
-  add_commit_push(paste0('Cria vista ', nome_vista, '.'))
+  add_commit_push(paste0('Zelador sobe ', arq_selecionado, ' de ', username, '.'), pedir_pull_request = TRUE)
 }
 
 
 # Execução --------------------------------------------------------------------------------------------------------
-if(faz_executa) {
+if(faz_executa_zelador) {
 # Prepara ---------------------------------------------------------------------------------------------------------
-  console('Sigtap Omop, Script Zelador versão 1.1')
+  console('Sigtap Omop, Script Zelador versão ', no_versão_zelador)
   console('Por Fabrício Kury (fab@kury.dev) e Carlos Campos (cl@precisiondata.com.br), agosto de 2.022')
   if(!file.exists(dolt_exe))
     stop(paste0('Não foi possível encontrar o Dolt. Favor verificar a instalação.\n',
@@ -658,27 +615,24 @@ if(faz_executa) {
   carrega_bibliotecas()
 
 # Config ----------------------------------------------------------------------------------------------------------
-  if(!file.exists(arq_config))
-    stop(paste0('Não foi possível encontrar o arquivo config.txt. Favor verificar a instalação.\n',
-      'Arquivo não encontrado:\n', arq_config))
   lê_config(arq_config)
   
 # Ciclo principal -------------------------------------------------------------------------------------------------
+  já_comunicou_login_detectado <- FALSE
   while(TRUE) {
     tem_credenciais <- verifica_creds()
     
     comandos <- c('Fazer login ao DoltHub')
     
     if(tem_credenciais) {
-      if(!exists('user_name')) {
-        user_name <- dolt('config', '--get', 'user.name')
-        user_name <- substr(user_name, 1, nchar(user_name)-1) # Elimina o \n que aparece no final.
-        console('Login no DoltHub detectado para ', user_name, '.\n')
+      if(!já_comunicou_login_detectado) {
+        console('Login no DoltHub detectado para ', username, '.\n')
+        já_comunicou_login_detectado <- TRUE
       }
       comandos <- c(comandos,
         'Fazer fork',
         'Baixar linhas para mapear',
-        'Rafazer dolt push',
+        'Criar pull request',
         'Subir linhas mapeadas')
     }
     
@@ -696,13 +650,14 @@ if(faz_executa) {
     if(comando == 'Baixar linhas para mapear')
       baixa_linhas()
     
-    if(comando == 'Rafazer dolt push')
-      refazer_push()
+    if(comando == 'Criar pull request')
+      pede_pull_request()
     
     if(comando == 'Subir linhas mapeadas')
       sobe_linhas()
   }
   
   console('Saindo do script Zelador.')
+  fecha_dolt_sql()
 }
 
